8bf0a2dd032673a46fbcd2e1ce421b82fdd470f9
Egan Tamashiro <egantamashiro@gmail.com>
2016-01-25 21:35:51 -0800

    commit renames component folders to new more descriptive folder names

--- client/public/components/index.jsx
+++ client/public/components/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './components/App';
+import App from './indexComponents/App';
 
 ReactDOM.render(
     <App />,
--- client/public/components/indexComponents/appComponents/LandingPageContent.js
+++ client/public/components/indexComponents/appComponents/LandingPageContent.js
@@ -3,8 +3,8 @@ import React from 'react';
 export default React.createClass({
 
   render: function() {
-    return <div className = "app">
-      <h1>!HELLO WORLD!</h1>
+    return <div className = "AppContainer">
+      <h1>Hello World</h1>
     </div>
   }
 
