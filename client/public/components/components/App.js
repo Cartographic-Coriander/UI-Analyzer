21f3c6d19e1922cb4c0edbc50a60782ec046cae9
Egan Tamashiro <egantamashiro@gmail.com>
2016-01-25 21:16:45 -0800

    renders hello world on screen (yay) in react (yay)

---
+++ client/public/components/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default React.createClass({
+
+  render: function() {
+    return <div className = "app">
+      <h1>!HELLO WORLD!</h1>
+    </div>
+  }
+
+});
--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,31 @@
+var webpack = require('webpack');
+
 module.exports = {
   entry: [
-    './client/public/components/index.js'
+    'webpack-dev-server/client?http://localhost:8080',
+    'webpack/hot/only-dev-server',
+    './client/public/components/index.jsx'
   ],
-  output: {
+  output: {/*find the 'start' specified at entry and write the new bundle.js file to the directory below*/
     path: __dirname + '/client/public',
-    publicPath: './client/public',
+    publicPath: 'client/public',
     filename: 'bundle.js'
   },
   devServer: {
-    contentBase: './client/public'
-  }
+    contentBase: './client/public',
+    hot: true
+  },
+  module: { /*the below means that webpack will find jsx and js files and process both through babel*/
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /node_modules/,
+      loader: 'babel'
+    }]
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx']
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin()
+  ]
 };
