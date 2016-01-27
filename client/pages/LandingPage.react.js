89ee28934608e99cb30a0310e7a187ad72df7908
Egan Tamashiro <egantamashiro@gmail.com>
2016-01-27 11:39:28 -0800

    fixes comfusing landing page class name

--- client/pages/LandingPage.react.js
+++ client/pages/LandingPage.react.js
@@ -10,7 +10,7 @@ export default React.createClass({
 
   render() {
     return (
-      <div className = "AppContainer">
+      <div className = "LandingPage">
         <Header />
         <Login />
         <Registration />
