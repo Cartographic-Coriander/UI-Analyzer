fa3850234f55c831b184d44bb917032ca205ab5f
Egan Tamashiro <egantamashiro@gmail.com>
2016-01-26 19:42:58 -0800

    adding label for login component that makes it easier to see what is going on

--- client/components/indexComponents/appComponents/landingPageContentComponents/Login.js
+++ client/components/indexComponents/appComponents/landingPageContentComponents/Login.js
@@ -6,6 +6,7 @@ export default React.createClass({
     return (
       <div>
         <form>
+          <h3>I am the login component that will appear</h3>
           <input type="text" placeholder="enter email here"></input>
           <input type="text" placeholder="password input here"></input>
           <button type="submit">submit</button>
