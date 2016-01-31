60d0a7f1fce6db011ec21f02ec6110fd70ca7f3b
Egan Tamashiro <egantamashiro@gmail.com>
2016-01-30 22:22:08 -0800

    adds field for company User model expects

--- client/components/landingPageComponents/Registration.js
+++ client/components/landingPageComponents/Registration.js
@@ -1,7 +1,7 @@
 import React, { Component, PropTypes } from 'react';
 import { reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
-export const fields = ['firstName', 'lastName', 'emailField', 'passwordField'];
+export const fields = ['firstName', 'lastName', 'company', 'emailField', 'passwordField'];
 
 const validate = values => {
   const errors = {};
@@ -22,7 +22,7 @@ const validate = values => {
 
 class SignUpForm extends Component{
   render () {
-  const { fields: { firstName, lastName, emailField, passwordField }, handleSubmit, submitting } = this.props;
+  const { fields: { firstName, lastName, company, emailField, passwordField }, handleSubmit, submitting } = this.props;
     return (
     <div>
       <h3>I am the registration component</h3>
@@ -42,6 +42,13 @@ class SignUpForm extends Component{
             <input type="text" placeholder="(+-_-) last name" {...lastName}/>
           </div>
           {lastName.touched && lastName.error && <div>{lastName.error}</div>}
+        </div>
+        <div>
+          <label>company</label>
+          <div>
+            <input type="text" placeholder="company ( ;_;)" {...company}/>
+          </div>
+          {company.touched && company.error && <div>{company.error}</div>}
         </div>
         <div>
           <label>enter e-mail</label>
