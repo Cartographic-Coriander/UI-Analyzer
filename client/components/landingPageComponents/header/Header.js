1d1cceb8b7e524420a947d1138ff9340b3ed3ee4
jotakwak <ykwak323@gmail.com>
2016-01-31 19:13:17 -0800

    Add styling to the landing page header

--- client/components/landingPageComponents/header/Header.js
+++ client/components/landingPageComponents/header/Header.js
@@ -4,11 +4,10 @@ class Header extends Component {
 
   render() {
     return (
-      <div className="headerComponent">
-        <h3>the header component</h3>
-        <button className="getStartedButton" onClick={ () => this.props.authenticateClick('authenticated') }>Get Started</button>
-        <button className="loginButton">Login</button>
-      </div>
+      <ul className="headerComponent">
+          <li className="getStartedButton" onClick={ () => this.props.authenticateClick('authenticated') }> <a href="#">Get Started</a></li>
+          <li className="loginButton"><a href="#">Log In</a></li>
+      </ul>
       )
   }
 }; 
