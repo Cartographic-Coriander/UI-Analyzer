00556c4020cdf6fe4802514e010cd21910ad22d5
Egan Tamashiro <egantamashiro@gmail.com>
2016-02-05 10:54:53 -0800

    commit adds modal that shows up and adds test TODO: add project should be in a modal as well, and the current GET to populate state.tests.list with relevant tests is not working

--- client/components/dashboardPageComponents/contentComponents/testContainer/TestContainer.js
+++ client/components/dashboardPageComponents/contentComponents/testContainer/TestContainer.js
@@ -1,35 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import TestContainerEntry from './subComponents/TestContainerEntry';
-import { deletesTest, updatesTest } from '../../../../redux/actions';
-
-let fakeTest = [
-  {
-    id: 1,
-    name: 'test one',
-    prompt: 'this is prompt for test one',
-    projectId: 1,
-    url: 'www.url.com'
-  },
-  {
-    id: 2,
-    name: 'test Two',
-    prompt: 'this is prompt for test two',
-    projectId: 2,
-    url: 'www.url.com'
-  },
-  {
-    id: 3,
-    name: 'test Three',
-    prompt: 'this is prompt for test thres',
-    projectId: 3,
-    url: 'www.url.com'
-  }
-];
-
+import { deletesTest, updatesTest, postsTest } from '../../../../redux/actions';
+import { Modal } from 'react-bootstrap';
 
 class TestContainer extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      testModalDisplay: false,
+      addTestName : null,
+      addTestPrompt: null,
+      addTestUrl: null
+    };
+  };
 
+  //editing existing tests
   updateTest (test) {
     this.props.dispatch(updatesTest(test));
   };
@@ -38,15 +24,65 @@ class TestContainer extends Component {
     this.props.dispatch(deletesTest(test));
   };
 
+  //adding new tests
+  addTest (test) {
+    let newTest = {
+      projectId: this.props.currentFocus.projectId,
+      name: this.state.addTestName,
+      url: this.state.addTestUrl,
+      prompt: this.state.addTestPrompt 
+    };
+    this.hideModal();
+    console.log(newTest);
+    this.props.dispatch(postsTest(newTest));
+  };
+
+  newTestName (event) {
+    this.setState({ addTestName : event.target.value });
+  };
+
+  newTestPrompt (event) {
+    this.setState({ addTestPrompt : event.target.value });
+  };
+
+  newTestUrl (event) {
+    this.setState({ addTestUrl : event.target.value });
+  };
+
+  //for showing and hiding modals
+  toggleModal () {
+    this.setState({
+      testModalDisplay : this.state.testModalDisplay === true ? false : true
+    })
+  }
+
+  hideModal () {
+    this.setState({
+      testModalDisplay : false
+    })
+  }
+
   render () {
+
     return (
       <div className = "Tests">
         <h3>I am the test settings component</h3>
         <div>
-          { fakeTest.map(function (test) {
+          { this.props.tests.list.map(function (test) {
             return <TestContainerEntry update = { this.updateTest.bind(this) } delete = { this.deleteTest.bind(this) } key = { test.id } name = { test.name } prompt = { test.prompt } id = { test.id } projectId = { test.projectId } url = { test.url } />
           }.bind(this)) }
         </div>
+
+        <Modal show = { this.state.testModalDisplay }>
+          <form>
+            test name: <input onChange = { this.newTestName.bind(this) } type = "text"></input><br></br>
+            test prompt: <input onChange = { this.newTestPrompt.bind(this) } type = "text"></input><br></br>
+            test url: <input onChange = { this.newTestUrl.bind(this) } type = "text"></input><br></br>
+            <button onClick = { this.addTest.bind(this) } type = "button">submit</button> <button onClick = { this.hideModal.bind(this) } type= "button" >cancel</button>
+          </form>
+        </Modal>
+
+        <button onClick = { this.toggleModal.bind(this) } type= "button">add test</button>
       </div>
     )
   };
