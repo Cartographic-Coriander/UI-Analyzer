4f0eb064777425c68c2c5fde071362fe60c86ebe
M5150 <maxcudich@gmail.com>
2016-02-10 10:53:50 -0800

    complete headers on static served middleware

--- server/services/middleware.js
+++ server/services/middleware.js
@@ -5,6 +5,13 @@ var morgan = require('morgan');
 var session = require('express-session');
 
 module.exports = function (app, express) {
+  var staticServeOptions = {
+    setHeaders:
+      function (res, path, stat) {
+        res.set('Access-Control-Allow-Origin', '*');
+      }
+  };
+
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({ extended: true }));
   app.use(bodyParser.json({ limit: '50mb' }));
@@ -18,5 +25,5 @@ module.exports = function (app, express) {
   // session.
   app.use(auth.passport.initialize());
   app.use(auth.passport.session());
-  app.use(express.static(__dirname + '/../../client/public'));
+  app.use(express.static(__dirname + '/../../client/public', staticServeOptions));
 };
