4ba11e3545b5f53eefbb47bdac082ad6075aff26
Egan Tamashiro <egantamashiro@gmail.com>
2016-02-26 13:50:32 -0800

    landing page sp error and short blurb in aboutme section

--- client/components/landingPageComponents/aboutUs/AboutUs.js
+++ client/components/landingPageComponents/aboutUs/AboutUs.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import GroupMember from './subComponents/GroupMember';
 
-const corianderMembers = [{name: 'Maximo Cudich-Sieburger', description: 'Max resume personal'}, {name: 'Youngjun "Jota" Kwak', description: 'Jota resume personal'}, {name: 'Egan Tamashiro', description: 'Egan resume personal'}];
+const corianderMembers = [
+{name: 'Maximo Cudich-Sieburger', description: 'Max resume personal'}, 
+{name: 'Youngjun "Jota" Kwak', description: 'Jota resume personal'}, 
+{name: 'Egan Tamashiro', description: 'In addition to being a full-stack developer, Egan loves the ocean and everything about the game of baseball.'}];
 
 const groupMembers = corianderMembers.map((member) => {
-  return <GroupMember key = { member.name } name = { member.name } description = { member.description }/>
+  return <GroupMember key = { member.name } name = { member.name } description = { member.description } gitHub = { member.gitHub }/>
 });
 
 const AboutUs = React.createClass({
