3ee95ba3f6dc1aa6436c44b21c0486e27c528070
Egan Tamashiro <egantamashiro@gmail.com>
2016-02-26 12:47:12 -0800

    making dashboard smexy

--- client/components/dashboardPageComponents/contentComponents/dashboardContainer/DashboardContainer.js
+++ client/components/dashboardPageComponents/contentComponents/dashboardContainer/DashboardContainer.js
@@ -1,20 +1,29 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { Col, Panel } from 'react-bootstrap';
+import { Col, Panel, Button } from 'react-bootstrap';
 
 export default class DashboardContainer extends Component {
   render () {
     return (
       <div className = 'Dashboard'>
-        <Col md = { 9 } mdOffset = { 3 }>
+        <Col md = { 11 } mdOffset = { 3 }>
           <div className = "panel panel-default">
             <div className = "panel-body">
               <h3>Welcome to Scrutinize</h3>
             </div>
-            <div className = "panel-footer">
-              <h4>add some projects</h4>
-            </div>
           </div>
+          <Col md = { 6 }>
+            <div className = "panel panel-default dashboardLeftContainer">
+              <div className = "panel-body dashboardLeft">
+                <Button className = "btn btn-primary">add project</Button>
+              </div>
+            </div>
+          </Col>
+          <Col md = { 6 }>
+            <Panel header = "Projects" className = "dashboardRightContainer">
+              <h5>Use projects collect data on how users interact with your website</h5>
+            </Panel>
+          </Col>
         </Col>
       </div>
     );
